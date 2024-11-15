5c3b279a4aef387fe38f81a662dedbb9d23234ee
Rajveer Sehmbi <rs218@ic.ac.uk>
2024-11-15 16:56:21 +0000

    Added Spring Boot Framework

---
+++ Backend_REST_API/src/main/java/com/southmeadstores/Backend_REST_API/BackendRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.southmeadstores.Backend_REST_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRestApiApplication.class, args);
+	}
+
+}
---
+++ Backend_REST_API/src/test/java/com/southmeadstores/Backend_REST_API/BackendRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.southmeadstores.Backend_REST_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
